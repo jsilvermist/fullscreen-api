098f154dc3a0db7b3d83b2196473431e6016359c
Jsilvermist <jsilvermist@silverlinkz.net>
2018-05-17 00:06:41 -0400

    Change to use a module based approach

--- fullscreen-api.js
+++ fullscreen-api.js
@@ -1,99 +1,110 @@
-(function(document) {
-  'use strict';
+const apis = {
+  w3: {
+    enabled: "fullscreenEnabled",
+    element: "fullscreenElement",
+    request: "requestFullscreen",
+    exit:    "exitFullscreen",
+    events: {
+      change: "fullscreenchange",
+      error:  "fullscreenerror",
+    },
+  },
+  webkit: {
+    enabled: "webkitFullscreenEnabled",
+    element: "webkitCurrentFullScreenElement",
+    request: "webkitRequestFullscreen",
+    exit:    "webkitExitFullscreen",
+    events: {
+      change: "webkitfullscreenchange",
+      error:  "webkitfullscreenerror",
+    },
+  },
+  moz: {
+    enabled: "mozFullScreenEnabled",
+    element: "mozFullScreenElement",
+    request: "mozRequestFullScreen",
+    exit:    "mozCancelFullScreen",
+    events: {
+      change: "mozfullscreenchange",
+      error:  "mozfullscreenerror",
+    },
+  },
+  ms: {
+    enabled: "msFullscreenEnabled",
+    element: "msFullscreenElement",
+    request: "msRequestFullscreen",
+    exit:    "msExitFullscreen",
+    events: {
+      change: "MSFullscreenChange",
+      error:  "MSFullscreenError",
+    },
+  },
+};
 
-  const apis = {
-    w3: {
-      enabled: "fullscreenEnabled",
-      element: "fullscreenElement",
-      request: "requestFullscreen",
-      exit:    "exitFullscreen",
-      events: {
-        change: "fullscreenchange",
-        error:  "fullscreenerror"
-      }
-    },
-    webkit: {
-      enabled: "webkitFullscreenEnabled",
-      element: "webkitCurrentFullScreenElement",
-      request: "webkitRequestFullscreen",
-      exit:    "webkitExitFullscreen",
-      events: {
-        change: "webkitfullscreenchange",
-        error:  "webkitfullscreenerror"
-      }
-    },
-    moz: {
-      enabled: "mozFullScreenEnabled",
-      element: "mozFullScreenElement",
-      request: "mozRequestFullScreen",
-      exit:    "mozCancelFullScreen",
-      events: {
-        change: "mozfullscreenchange",
-        error:  "mozfullscreenerror"
-      }
-    },
-    ms: {
-      enabled: "msFullscreenEnabled",
-      element: "msFullscreenElement",
-      request: "msRequestFullscreen",
-      exit:    "msExitFullscreen",
-      events: {
-        change: "MSFullscreenChange",
-        error:  "MSFullscreenError"
-      }
-    }
-  };
-
-  const w3 = apis.w3;
-  let api = null;
-  for (const vendor in apis) {
-    if (apis[vendor].enabled in document) {
-      api = apis[vendor];
-      break;
-    }
+const w3 = apis.w3;
+let api = null;
+for (const vendor in apis) {
+  if (apis[vendor].enabled in document) {
+    api = apis[vendor];
+    break;
   }
+}
+
+const fullscreen = {};
+
+// Set initial static properties
+fullscreen[w3.enabled] = document[api.enabled];
+fullscreen[w3.element] = document[api.element];
+
+// Bind exit method
+fullscreen[w3.exit] = document[api.exit].bind(document);
+
+// Call request for fullscreen on element
+fullscreen[w3.request] = function(target) {
+  target[api.request].call(target);
+};
+
+// Check if target is active fullscreen element
+fullscreen.fullscreenActive = function(target) {
+  return this[w3.enabled] && this[w3.element] === target;
+};
+
+// Register Fullscreen API to document or custom-element
+fullscreen.registerFullscreen = function(target) {
+
+  target = target || document;
 
   if (api && api !== w3) {
 
-    document[w3.enabled] = document[api.enabled];
-    document[w3.element] = document[api.element];
+    target.addEventListener(api.events.change, (e) => {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
 
-    document[w3.exit] = document[api.exit];
+      // Update static properties on change
+      this[w3.enabled] = document[api.enabled];
+      this[w3.element] = document[api.element];
 
-    Element.prototype[w3.request] = function() {
-      this[api.request].call(this);
-    };
+      // [TODO]: What should this be dispatched on? target || document || by-function
+      target.dispatchEvent(new CustomEvent(w3.events.change, {detail:{originalEvent:e}}));
+    });
 
-    document.registerFullscreen = function(target) {
+    target.addEventListener(api.events.error, (e) => {
+      // [TODO]: What should this be dispatched on? target || document || by-function
+      target.dispatchEvent(new CustomEvent(w3.events.error, {detail:{originalEvent:e}}));
+    });
 
-      target = target || document;
+    return true;
 
-      target.addEventListener(api.events.change, (e) => {
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-
-        document[w3.enabled] = document[api.enabled];
-        document[w3.element] = document[api.element];
-
-        document.dispatchEvent(new Event(w3.events.change));
-      });
-
-      target.addEventListener(api.events.error, (e) => {
-        document.dispatchEvent(new Event(w3.events.error));
-      });
-
-      return true;
-    };
-
-    // document.body.registerFullscreen.call(document);
-    document.registerFullscreen();
+  // [TODO]: Support official w3 api here
+  // } else if (api && api === w3) {
+  //   return true;
 
   } else {
 
-    document.registerFullscreen = function() {
-      return false;
-    };
+    return false;
 
   }
 
-})(document);
+};
+
+export default fullscreen;
