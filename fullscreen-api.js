7fc4bde0fa6f77c98b0be9cba7fe00c7e4264dd5
Jsilvermist <jsilvermist@silverlinkz.net>
2018-05-18 13:05:47 -0400

    Use simple names in an object syntax

--- fullscreen-api.js
+++ fullscreen-api.js
@@ -50,78 +50,73 @@ for (const vendor in apis) {
   }
 }
 
-const fullscreen = {};
+export const fullscreen = {
 
-// Mirror initial static properties
-fullscreen[w3.enabled] = document[api.enabled];
-fullscreen[w3.element] = document[api.element];
+  // Mirror initial static properties
+  enabled: document[api.enabled] || false,
+  element: document[api.element] || null,
 
-// Bind exit method
-fullscreen[w3.exit] = document[api.exit].bind(document);
+  // Check if there is currently an active fullscreen element
+  get active() {
+    return this.enabled && this.element;
+  },
 
-// Call request for fullscreen on element
-fullscreen[w3.request] = function(target) {
-  target[api.request].call(target);
-};
+  // Call request for fullscreen on element
+  request(target) {
+    target[api.request].call(target);
+  },
 
-// Check if target is active fullscreen element
-fullscreen.fullscreenActive = function(target) {
-  return this[w3.enabled] && this[w3.element] === target;
-};
+  // Bind exit method
+  exit: document[api.exit].bind(document),
 
-// Register Fullscreen API to document or custom-element
-fullscreen.registerFullscreen = function(target) {
+  // Register Polyfill on document or custom-element
+  registerPolyfill(target) {
+    target = target || document;
 
-  target = target || document;
+    if (api && api !== w3) {
 
-  if (api && api !== w3) {
+      // [TODO]: Where should this dispatch the w3 event?
+      // target || document || by-callback
 
-    // [TODO]: Where should this dispatch the w3 event?
-    // target || document || by-callback
+      target.addEventListener(api.events.change, (e) => {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
 
-    target.addEventListener(api.events.change, (e) => {
-      e.stopPropagation();
-      e.stopImmediatePropagation();
+        // Update static properties on change
+        this.enabled = document[api.enabled];
+        this.element = document[api.element];
 
-      // Update static properties on change
-      this[w3.enabled] = document[api.enabled];
-      this[w3.element] = document[api.element];
+        document.dispatchEvent(new CustomEvent(w3.events.change, {
+          detail: { originalEvent: e }
+        }));
+      });
 
-      target.dispatchEvent(new CustomEvent(w3.events.change, {
-        detail: { originalEvent: e }
-      }));
-    });
+      target.addEventListener(api.events.error, (e) => {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
 
-    target.addEventListener(api.events.error, (e) => {
-      e.stopPropagation();
-      e.stopImmediatePropagation();
+        document.dispatchEvent(new CustomEvent(w3.events.error, {
+          detail: { originalEvent: e }
+        }));
+      });
 
-      target.dispatchEvent(new CustomEvent(w3.events.error, {
-        detail: { originalEvent: e }
-      }));
-    });
+      return true;
 
-    return true;
+    } else if (api && api === w3) {
 
-  } else if (api && api === w3) {
+      target.addEventListener(api.events.change, (e) => {
+        // Update static properties on change
+        this.enabled = document[api.enabled];
+        this.element = document[api.element];
+      });
 
-    // Mirror initial static properties
-    this[w3.enabled] = document[api.enabled];
-    this[w3.element] = document[api.element];
+      return true;
 
-    target.addEventListener(api.events.change, (e) => {
-      // Update static properties on change
-      this[w3.enabled] = document[api.enabled];
-      this[w3.element] = document[api.element];
-    });
+    } else {
 
-    return true;
-
-  } else {
-
-    return false;
-
-  }
+      return false;
+    }
+  },
 
 };
 
